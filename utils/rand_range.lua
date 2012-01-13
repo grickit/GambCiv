40946140231c48f65153bb3c6d4635fa0aa5d330
Derek Hoagland <thegrickit@gmail.com>
2012-01-13 16:21:12 -0500

    Fix season OOSing

--- utils/rand_range.lua
+++
@@ -1,5 +0,0 @@
-local function handler(t)
-  number = (wesnoth.synchronize_choice(function() return { result = math.random(t.lower,t.upper) } end).result)
-  wesnoth.set_variable(t.variable,number)
-end
-wesnoth.register_wml_action("rand_range", handler)
